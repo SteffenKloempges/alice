7d33e6cb01aeddf3657dd82607c8f7f03643801a
Steffen Klömpges <steffenklompges@MacBook-Air-von-Steffen-2.fritz.box>
2022-04-03 18:28:38 +0200

    adding accordion

--- assets/js/main.js
+++ assets/js/main.js
@@ -88,3 +88,22 @@ function slide() {
 setInterval(function () {
     slide()
 }, 5000);
+
+// ========================
+//      accordion
+// ========================
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
